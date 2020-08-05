e27b6969f856c8e9c4938fb4baaab3c706f88c86
erupakasaiakshay <erupakasaiakshay@gmail.com>
2020-08-05 11:32:37 -0400

    refactored and added score & result in quiz

--- app/src/main/java/com/example/frenchapp/Quiz.java
+++ app/src/main/java/com/example/frenchapp/Quiz.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -36,7 +37,9 @@ public class Quiz extends AppCompatActivity {
     private TextView question;
     private String hintt;
     private ProgressBar progressBar;
+    private TextView score;
     private LinearLayout quizcard,resultcard;
+    private String quiz_no;
 
     private int q_no = 1;
 
@@ -47,6 +50,12 @@ public class Quiz extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        Intent in = getIntent();
+        quiz_no = in.getStringExtra("quiz_no");
+
+        if(quiz_no.equals("")){
+            finish();
+        }
 
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -66,6 +75,7 @@ public class Quiz extends AppCompatActivity {
         progressBar = findViewById(R.id.progress_bar);
         quizcard = findViewById(R.id.quizcard);
         resultcard = findViewById(R.id.resultcard);
+        score = findViewById(R.id.score);
 
         resultcard.setVisibility(View.GONE);
         quizcard.setVisibility(View.VISIBLE);
@@ -75,6 +85,7 @@ public class Quiz extends AppCompatActivity {
         op3 = findViewById(R.id.op3);
 
         hint = findViewById(R.id.hint);
+        hint.setVisibility(View.VISIBLE);
 
         showQuestion(q_no);
 
@@ -82,73 +93,77 @@ public class Quiz extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(hintt!=null){
-                new AlertDialog.Builder(Quiz.this)
-                        .setTitle("Hint !!")
-                        .setMessage(hintt)
+                    new AlertDialog.Builder(Quiz.this)
+                            .setTitle("Hint !!")
+                            .setMessage(hintt)
 
-                        // Specifying a listener allows you to take an action before dismissing the dialog.
-                        // The dialog is automatically dismissed when a dialog button is clicked.
-                        .setPositiveButton("GOT IT!!", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                            }
-                        }).setIcon(android.R.drawable.ic_dialog_alert)
-                        .show();
-            }}
+                            // Specifying a listener allows you to take an action before dismissing the dialog.
+                            // The dialog is automatically dismissed when a dialog button is clicked.
+                            .setPositiveButton("GOT IT!!", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                }
+                            }).setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }}
         });
 
 
     }
 
     private void showQuestion(int no){
-        if(no==10){
+        if(no==11){
+            score.setText(count_correct+"/10");
             resultcard.setVisibility(View.VISIBLE);
             quizcard.setVisibility(View.GONE);
+            hint.setVisibility(View.GONE);
+            mDatabase.child("Score").child(mAuth.getUid()).
 
         }
-        progressBar.setProgress(progressBar.getProgress()+10);
-        question_no.setText("Question "+no);
-        YoYo.with(Techniques.SlideInRight)
-                .duration(700)
-                .playOn(findViewById(R.id.quizcard));
+        else {
+            progressBar.setProgress(progressBar.getProgress() + 10);
+            question_no.setText("Question " + no);
+            YoYo.with(Techniques.SlideInRight)
+                    .duration(700)
+                    .playOn(findViewById(R.id.quizcard));
 
-        mDatabase.child("quizes").child("quiz1").child("q"+no).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                final Question q = snapshot.getValue(Question.class);
-                question.setText(q.getQuestion());
-                op1.setText(q.getOp1());
-                op2.setText(q.getOp2());
-                op3.setText(q.getOp3());
-                hintt = q.getHint();
+            mDatabase.child("quizes").child(quiz_no).child("q" + no).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    final Question q = snapshot.getValue(Question.class);
+                    question.setText(q.getQuestion());
+                    op1.setText(q.getOp1());
+                    op2.setText(q.getOp2());
+                    op3.setText(q.getOp3());
+                    hintt = q.getHint();
 
-                op1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        checkAnswer(1,q,op1);
-                    }
-                });
-                op2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        checkAnswer(2,q,op2);
-                    }
-                });
-                op3.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        checkAnswer(3,q,op3);
-                    }
-                });
+                    op1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            checkAnswer(1, q, op1);
+                        }
+                    });
+                    op2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            checkAnswer(2, q, op2);
+                        }
+                    });
+                    op3.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            checkAnswer(3, q, op3);
+                        }
+                    });
 
-            }
+                }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(Quiz.this,error.getMessage(),Toast.LENGTH_LONG).show();
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(Quiz.this, error.getMessage(), Toast.LENGTH_LONG).show();
 
-            }
-        });
-
+                }
+            });
+        }
 
     }
 
@@ -158,6 +173,7 @@ public class Quiz extends AppCompatActivity {
             btn.setBackground(getResources().getDrawable(R.drawable.btn_green));
             btn.setEnabled(false);
             btn.setTextColor(getResources().getColor(R.color.green));
+            count_correct++;
 
         }
         else{
